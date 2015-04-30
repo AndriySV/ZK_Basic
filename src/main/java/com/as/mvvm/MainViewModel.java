f4ff638d30abca8a35afd860760d2ee370266e3c
AndriySV <svyryd.andriy@gmail.com>
2015-04-30 19:25:08 +0300

    test

--- src/main/java/com/as/mvvm/MainViewModel.java
+++ src/main/java/com/as/mvvm/MainViewModel.java
@@ -6,6 +6,7 @@ import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
 
+// Test
 public class MainViewModel implements Serializable {
 
 	@Init
